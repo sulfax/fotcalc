44edeb0b075976d0d6a552c63fc53bde891a427f
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-03-31 20:26:39 +0200

    Add files via upload

--- static/forside.js
+++ static/forside.js
@@ -314,10 +314,8 @@ var Lag3 = {
 var schema = {
   "@context": "https://schema.org",
   "@type": "FAQPage",
-  "mainEntity": []
+  "mainEntity": [Lag1,Lag2]
 }
-schema.mainEntity += (Lag1)
-schema.mainEntity += (',' + Lag2)
 schema.mainEntity += (',' + Lag3)
 const script = document.createElement('script');
 script.setAttribute('type', 'application/ld+json');
