b2ae9a0e81dad99085476a38652a5bfd42b4240f
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-04-13 11:25:59 +0200

    Add files via upload

--- static/country-coefficients.js
+++ static/country-coefficients.js
@@ -1,5 +1,43 @@
-
 let opp_ned_pil = '<span class="høyrestill"><img src="media/opp_ned_pil.svg" alt="Sorting arrows"></span>'
+
+
+
+let ranking_array = []
+testTabell = document.getElementById('minTest')
+
+var klubbers_assosiasjon = []
+for (i = 0; i < menyvalg.length; i++) {
+  klubbers_assosiasjon.push(menyvalg[i][1])
+}
+for (i = 0; i < landskoeffisienter.length; i++) {
+  let assos_ranking_array = []
+  let indeks = 0
+  let indeks_klubb = []
+  do {
+    indeks_klubb.push(klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks))
+    indeks = klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks) + 1
+  }
+  while (klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks) != -1)
+
+  let enkelt_sesong = 0
+  for (p = 0; p < indeks_klubb.length; p++) {
+    enkelt_sesong += (menyvalg[(indeks_klubb[p])][8]/indeks_klubb.length)
+  }
+
+  assos_ranking_array.push(landskoeffisienter[i][0])
+  assos_ranking_array.push((landskoeffisienter[i][1] + landskoeffisienter[i][2] + landskoeffisienter[i][3] + landskoeffisienter[i][4] + enkelt_sesong).toFixed(3))
+  assos_ranking_array.push(enkelt_sesong.toFixed(3))
+  assos_ranking_array.push(landskoeffisienter[i][4].toFixed(3))
+  assos_ranking_array.push(landskoeffisienter[i][3].toFixed(3))
+  assos_ranking_array.push(landskoeffisienter[i][2].toFixed(3))
+  assos_ranking_array.push(landskoeffisienter[i][1].toFixed(3))
+  ranking_array.push(assos_ranking_array)
+}
+
+
+
+
+
 
 $('th').on('click', function(){
   var column = $(this).data('column')
@@ -13,6 +51,14 @@ $('th').on('click', function(){
   }
   sorter(column, order, tekst, ranking_array)
 })
+
+sorter_etter_sesong()
+function sorter_etter_sesong() {
+  let column = localStorage.getItem('kolonne_landskoeffisient') || 'poeng'
+  let order = localStorage.getItem('rekkefølge_landskoeffisient') || 'desc'
+  let tekst = document.getElementById(column).innerText
+  sorter(column, order, tekst, ranking_array)
+}
 
 function sorter(column, order, tekst, ranking_array) {
   if (column == 'poeng') {
@@ -63,17 +109,26 @@ function sorter(column, order, tekst, ranking_array) {
     endre_kolonne_overskrift('sesong3', opp_ned_pil)
     endre_kolonne_overskrift('sesong4', opp_ned_pil)
   }
-  ranking_array.sort(sortFunction_tall_flere_desimal_nyligste);
   if(order == 'desc') {
+    if (column == 'poeng') {
+      for (p = 6; p > 1; p--) {
+        ranking_array.sort(sortFunction_tall_1_flere_desimal_nyligste);
+      }
+    }
     ranking_array.sort(sortFunction_tall_1_flere_desimal);
     tekst += '<span class="høyrestill"><img src="media/opp_NEDpil.svg" alt="Sorting arrows"></span>'
   }
   else {
+    if (column == 'poeng') {
+      for (p = 6; p > 1; p--) {
+        ranking_array.sort(sortFunction_tall_2_flere_desimal_nyligste);
+      }
+    }
     ranking_array.sort(sortFunction_tall_2_flere_desimal);
     tekst += '<span class="høyrestill"><img src="media/OPPned_pil.svg" alt="Sorting arrows"></span>'
   }
-  localStorage.setItem('kolonne', column)
-  localStorage.setItem('rekkefølge', order)
+  localStorage.setItem('kolonne_landskoeffisient', column)
+  localStorage.setItem('rekkefølge_landskoeffisient', order)
   document.getElementById(column).innerHTML = tekst;
   byggTabell_test(ranking_array)
 }
@@ -82,7 +137,7 @@ function sorter(column, order, tekst, ranking_array) {
 
 function endre_kolonne_overskrift(kolonne, opp_ned_pil) {
   if (document.getElementById(kolonne).innerHTML.replace(document.getElementById(kolonne).innerText, '') != opp_ned_pil) {
-    document.getElementById(kolonne).innerHTML = '<span id="premiepenger_navn">' + document.getElementById(kolonne).innerText + '</span>' + opp_ned_pil
+    document.getElementById(kolonne).innerHTML = document.getElementById(kolonne).innerText + opp_ned_pil
   }
 }
 
@@ -106,47 +161,23 @@ function sortFunction_tall_2_flere_desimal(a, b) {
   }
 }
 
-function sortFunction_tall_flere_desimal_nyligste(a, b) {
-  if (parseFloat(a[2]) === parseFloat(b[2])) {
+function sortFunction_tall_1_flere_desimal_nyligste(a, b) {
+  if (parseFloat(a[p]) === parseFloat(b[p])) {
     return 0;
   }
   else {
-    return (parseFloat(a[2]) > parseFloat(b[2])) ? -1 : 1;
+    return (parseFloat(a[p]) > parseFloat(b[p])) ? -1 : 1;
+  }
+}
+function sortFunction_tall_2_flere_desimal_nyligste(a, b) {
+  if (parseFloat(a[p]) === parseFloat(b[p])) {
+    return 0;
+  }
+  else {
+    return (parseFloat(a[p]) < parseFloat(b[p])) ? -1 : 1;
   }
 }
 
-
-let ranking_array = []
-testTabell = document.getElementById('minTest')
-
-var klubbers_assosiasjon = []
-for (i = 0; i < menyvalg.length; i++) {
-  klubbers_assosiasjon.push(menyvalg[i][1])
-}
-for (i = 0; i < landskoeffisienter.length; i++) {
-  let assos_ranking_array = []
-  let indeks = 0
-  let indeks_klubb = []
-  do {
-    indeks_klubb.push(klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks))
-    indeks = klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks) + 1
-  }
-  while (klubbers_assosiasjon.indexOf(landskoeffisienter[i][0], indeks) != -1)
-
-  let enkelt_sesong = 0
-  for (p = 0; p < indeks_klubb.length; p++) {
-    enkelt_sesong += (menyvalg[(indeks_klubb[p])][8]/indeks_klubb.length)
-  }
-
-  assos_ranking_array.push(landskoeffisienter[i][0])
-  assos_ranking_array.push((landskoeffisienter[i][1] + landskoeffisienter[i][2] + landskoeffisienter[i][3] + landskoeffisienter[i][4] + enkelt_sesong).toFixed(3))
-  assos_ranking_array.push(enkelt_sesong.toFixed(3))
-  assos_ranking_array.push(landskoeffisienter[i][1].toFixed(3))
-  assos_ranking_array.push(landskoeffisienter[i][2].toFixed(3))
-  assos_ranking_array.push(landskoeffisienter[i][3].toFixed(3))
-  assos_ranking_array.push(landskoeffisienter[i][4].toFixed(3))
-  ranking_array.push(assos_ranking_array)
-}
 
 
 // Mange fine flaggikoner: https://github.com/HatScripts/circle-flags
