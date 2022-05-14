8830988aee2bd71342267e4ced33f4fdc93188b5
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-05-14 14:43:23 +0200

    Add files via upload

--- static/club-coefficients.js
+++ static/club-coefficients.js
@@ -212,15 +212,39 @@ function oppdater_ved_refresh() {
 }
 
 
+var schema = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "mainEntity": []
+}
+var Lag_premiepenger = {
+    "@type": "Question",
+    "name": "How much prize money has PROMP earned so far?",
+    "acceptedAnswer": {
+      "@type": "Answer",
+      "text": "€ 15",
+    }
+}
+schema.mainEntity.push(Lag_premiepenger)
+const script = document.createElement('script');
+script.setAttribute('type', 'application/ld+json');
+script.textContent = JSON.stringify(schema);
+document.head.appendChild(script);
+
+
 // Endre meta-beskrivelsene
 // var descval = document.getElementById('tabell_hoved').innerText;
 
 const table = document.querySelector('table')
 const arr = [...table.rows].map(r => [...r.querySelectorAll('td, th')].map(td => td.textContent))
+arr[0][3] = "Country"
+arr[0][6] = arr[0][6].slice(0, -1)
+arr[0][7] = arr[0][7].slice(0, -1)
+arr[0][8] = arr[0][8].slice(0, -1)
+arr[0][9] = arr[0][9].slice(0, -1)
+arr[0][10] = arr[0][10].slice(0, -1)
 for (i = 0; i < arr.length; i++) {
   arr[i].splice(0,1)
-  arr[i].splice(2,1)
-  arr[i].splice(4,5)
 }
 var link = document.createElement('meta');  link.setAttribute('name', 'description');  link.content = arr; document.getElementsByTagName('head')[0].appendChild(link);
 
