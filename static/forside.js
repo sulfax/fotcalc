2186515df3315d1bd944f9e8fdea162a26c58112
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-08-27 14:45:57 +0200

    Add files via upload

--- static/forside.js
+++ static/forside.js
@@ -576,7 +576,7 @@ function byggTabell_test(data) {
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `6.png 70w,
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `7.png 100w,
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `8.png 140w"
-      data-fallback="media/klubblogo/fallback.png" sizes="19px" srcset="
+      data-fallback="media/klubblogo/fallback.png" alt="` + klubbnavn + `" sizes="19px" srcset="
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `1.png 18w,
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `2.png 32w,
       media/klubblogo/` + data[i][9] + "/" + klubbnavn_url + `3.png 36w,
