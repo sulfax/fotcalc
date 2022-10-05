d276b58ecc5ba42ea08a7bceb45134e9f638f312
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-10-05 20:46:41 +0200

    Add files via upload

--- static/forside.js
+++ static/forside.js
@@ -1533,7 +1533,8 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("ad_venstre").style.top = -scrolled + "px";
+  document.getElementById("ad_venstre").style.top = -3*scrolled + "px";
+  document.getElementById("ad_hoyre").style.top = -3*scrolled + "px";
 }
 
 // function endre_klubbnavn(i, kolonne) {
