5b87b0172c6942fb8968bea06dd3fe7c91127f40
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2022-10-07 15:04:41 +0200

    Add files via upload

--- billettsalg.js
+++ billettsalg.js
@@ -120,8 +120,7 @@ function generer_kalender(klubb) {
             /* Loopen må være der av en merkelig grunn */
             for (p = 0; p < 1; p++) {}
             var request = makeHttpObject();
-            request.header("Access-Control-Allow-Origin",'https://www.fotcalc.com/billettsalg')
-            request.open("GET", 'https://ticketco.events/no/nb/events/' + kamper[h][p] + '/seating_arrangement/', true);
+            request.open("GET", 'https://cors-anywhere.herokuapp.com/https://ticketco.events/no/nb/events/' + kamper[h][p] + '/seating_arrangement/', true);
             request.send(null);
             var kamptittel = "";
             f = h;
