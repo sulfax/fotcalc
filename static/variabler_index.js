e6e579fef9d4c64bb49f4e69ebe09524f2d1a72c
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2023-01-01 16:52:18 +0100

    Add files via upload

--- static/variabler_index.js
+++ static/variabler_index.js
@@ -156,8 +156,8 @@ const norsk_landskoeffisient = [
 
 const norsk_klubbkoeffisient = [
     'UEFA 5 års klubb koeffisient-rangering',
-    'UEFA klubbkoeffisient-rangering',
-    'Denne 5 års rangeringen bestemmer seeding og pot-fordeling i<br>Europakvalikk og gruppespill. En høyere rangering gir lettere<br>motstand i de fasene av en Europasesong.<br><br><span class="gjennomsiktighet">Rangeringen baseres på <a class="link_utydelig" href="coefficient-definition#club">klubb-koeffisientpoeng</a>.<br>Se også <a class="link_utydelig" href="10-year-ranking">10 års koeffisient rangeringen</a>.</span><br>',
+    'Klubbkoeffisient-rangering',
+    'Denne UEFA 5 års rangeringen bestemmer seeding og pot-fordeling i<br>Europakvalikk og gruppespill. En høyere rangering gir lettere<br>motstand i de fasene av en Europasesong.<br><br><span class="gjennomsiktighet">Rangeringen baseres på <a class="link_utydelig" href="coefficient-definition#club">klubb-koeffisientpoeng</a>.<br>Se også <a class="link_utydelig" href="10-year-ranking">10 års koeffisient rangeringen</a>.</span><br>',
     'Avgjørende for klubbutplassering i ',
     'Sjekk ut',
     '<span class="marketPool" id="oversikt_reklame"><b><a class="graa_hover_link" id="klubb_link" href="https://www.fotcalc.com/" onclick="endre_sort_kolonne()">Sammenlign klubbers koeffisientpoeng</a>.</b></span>'
