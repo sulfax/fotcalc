a9724afe35c1e9bc16618488660fb3683c0c2962
jkaste03 <94053898+jkaste03@users.noreply.github.com>
2023-02-15 21:08:22 +0100

    Add files via upload

--- static/variabler_index.js
+++ static/variabler_index.js
@@ -97,7 +97,7 @@ const norsk_landskoeffisient = [
     'UEFA Rangeringen av et land bestemmer hvor mange<br>Europaplasser dem vil få og når i turneringene deres klubber vil starte sin deltagelse.<br><br><span class="gjennomsiktighet"><a class="link_utydelig" href="coefficient-definition#association">Landskoeffisienter</a> er også kjent som "Assosiasjons-klubbkoeffisient".</span>',
     'Rangering av klubber basert på bidratte lands-koeffisientpoeng til eget land:',
     'Avgjørende for klubbutplassering i ',
-    'Klubber med blå bakgrunnsfarge er fortsatt i konkurranse. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
+    'Klubber med blå bakgrunnsfarge konkurrerer fortsatt. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
     'Sjekk ut',
     '<span class="marketPool" id="oversikt_reklame"><b><a class="graa_hover_link" id="klubb_link" href="https://www.fotcalc.com/" onclick="endre_sort_kolonne()">Sammenlign klubbers koeffisientpoeng</a>.</b></span>',
     '<abbr data_title="England"><img class="flagg" src="media/UEFA/ENG.svg"></abbr>',
@@ -162,7 +162,7 @@ const norsk_klubbkoeffisient = [
     'Klubbkoeffisient-rangering',
     'Denne UEFA 5 års rangeringen bestemmer seeding og pot-fordeling i<br>Europakvalikk og gruppespill. En høyere rangering gir lettere<br>motstand i de fasene av en Europasesong.<br><br><span class="gjennomsiktighet">Rangeringen baseres på <a class="link_utydelig" href="coefficient-definition#club">klubb-koeffisientpoeng</a>.<br>Se også <a class="link_utydelig" href="10-year-ranking">10 års koeffisient rangeringen</a>.</span><br>',
     'Avgjørende for klubbutplassering i ',
-    'Klubber med blå bakgrunnsfarge er fortsatt i konkurranse. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
+    'Klubber med blå bakgrunnsfarge konkurrerer fortsatt. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
     'Sjekk ut',
     '<span class="marketPool" id="oversikt_reklame"><b><a class="graa_hover_link" id="klubb_link" href="https://www.fotcalc.com/" onclick="endre_sort_kolonne()">Sammenlign klubbers koeffisientpoeng</a>.</b></span>'
 ];
@@ -172,7 +172,7 @@ const norsk_ti_års = [
     '10 års koeffisient-rangering',
     'Denne rangeringen benyttes kun til å finne 10 års koeffisient-bonusen (€)<br>til klubbene som deltar i europeisk gruppespill.<br><br><span class="gjennomsiktighet">Rangeringen baseres på <a class="link_utydelig" href="coefficient-definition#club">klubb-koeffisientpoeng</a>.<br>Se også <a class="link_utydelig" href="club-coefficients">5 års koeffiesient rangeringen</a>.</span>',
     'Avgjørende for gruppespills-bonus (€) i ',
-    'Klubber med blå bakgrunnsfarge er fortsatt i konkurranse. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
+    'Klubber med blå bakgrunnsfarge konkurrerer fortsatt. Klubber med grønn bakgrunnsfarge er fra aktive nasjoner.',
     'Sjekk ut',
     '<span class="marketPool" id="oversikt_reklame"><b><a class="graa_hover_link" id="klubb_link" href="https://www.fotcalc.com/" onclick="endre_sort_kolonne()">Sammenlign klubbers koeffisientpoeng</a>.</b></span>'
 ]
