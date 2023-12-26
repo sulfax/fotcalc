a8f73e27ff40803ae00ae22e5db4525de3db409d
jkaste03 <johanneskaste@gmail.com>
2023-12-27 00:53:58 +0100

    Fiks

--- static/club-coefficients.js
+++ static/club-coefficients.js
@@ -1584,8 +1584,8 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("ad_venstre").style.top = -3*scrolled + "px";
-  document.getElementById("ad_hoyre").style.top = -3*scrolled + "px";
+  document.getElementById("ad_venstre").style.top = -1.5*scrolled + "px";
+  document.getElementById("ad_hoyre").style.top = -1.5*scrolled + "px";
 }
 
 
