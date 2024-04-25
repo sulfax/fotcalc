f56ff74f00a94c2ff96dcfa3974c9ba6f2c5fb62
jkaste03 <johanneskaste@gmail.com>
2024-04-25 19:58:20 +0200

    Fiks

--- static/index.js
+++ static/index.js
@@ -1,25 +1,25 @@
-var $root = $('html, body');
-$('a[href^="#"]').click(function(event) {
-	var href = $.attr(this, 'href');
-	// Sjekk om lenken er bare "#" 
-	if (href === "#") {
-		// Gjeldende logikk for smooth scrolling
-		$root.animate({
-			scrollTop: $("#navbar").offset().top
-	}, 500, function () {
-			window.location.hash = "#";
-			history.replaceState('', document.title, window.location.pathname + window.location.search);
-	});
-	} else {
-			// Gjeldende logikk for smooth scrolling
-			$root.animate({
-					scrollTop: $(href).offset().top
-			}, 500, function () {
-					window.location.hash = href;
-			});
-	}
-	event.preventDefault();
-});
+// var $root = $('html, body');
+// $('a[href^="#"]').click(function(event) {
+// 	var href = $.attr(this, 'href');
+// 	// Sjekk om lenken er bare "#" 
+// 	if (href === "#") {
+// 		// Gjeldende logikk for smooth scrolling
+// 		$root.animate({
+// 			scrollTop: $("#navbar").offset().top
+// 	}, 500, function () {
+// 			window.location.hash = "#";
+// 			history.replaceState('', document.title, window.location.pathname + window.location.search);
+// 	});
+// 	} else {
+// 			// Gjeldende logikk for smooth scrolling
+// 			$root.animate({
+// 					scrollTop: $(href).offset().top
+// 			}, 500, function () {
+// 					window.location.hash = href;
+// 			});
+// 	}
+// 	event.preventDefault();
+// });
 
 var eksperimentell_profil_e = "Calculate from scratch";
 var eksperimentell_profil_n = "Kalkuler fra bunnen";
